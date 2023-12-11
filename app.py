0f23c80482a1bbab1c3378bd7fbdd2d5d8d37118
saurav <tessco913@gmail.com>
2023-12-11 12:15:02 +0530

    Completed all the basic initialization for the flask app and html files.

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
+
+# basic setup
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "hehe"
+socketio = SocketIO(app)
+
+
+# index page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+# run server
+socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
