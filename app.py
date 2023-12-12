c83ce492691be00f458b86e554841fd0d83e146b
saurav <tessco913@gmail.com>
2023-12-12 11:57:48 +0530

    handled some routes and made some minore changes.

--- app.py
+++ app.py
@@ -12,7 +12,25 @@ socketio = SocketIO(app)
 # index page
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join_button = request.form.get("join_button", False)
+        create_button = request.form.get("create_button", False)
+
+    print(request.form)
     return render_template("index.html")
+
+
+@app.route("/failure", methods=["GET", "POST"])
+def failure():
+    fail_string = "No fail string."
+    if request.method == "POST":
+        back_button = request.form.get("back_button", False)
+
+        if back_button == "1":
+            return redirect("/")
+    return render_template("failure.html", fail_string=fail_string)
 
 
 # run server
